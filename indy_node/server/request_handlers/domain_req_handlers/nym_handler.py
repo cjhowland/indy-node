eda275bc99c0386cb8e8d523cc00c43372a9984f
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-08-17 11:41:47 +0200

    check is_self_certifying shell
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- indy_node/server/request_handlers/domain_req_handlers/nym_handler.py
+++ indy_node/server/request_handlers/domain_req_handlers/nym_handler.py
@@ -11,7 +11,13 @@ from ledger.util import F
 from plenum.common.constants import ROLE, TARGET_NYM, VERKEY, TXN_TIME
 from plenum.common.exceptions import InvalidClientRequest
 from plenum.common.request import Request
-from plenum.common.txn_util import get_payload_data, get_seq_no, get_txn_time, get_request_data, get_from
+from plenum.common.txn_util import (
+    get_payload_data,
+    get_seq_no,
+    get_txn_time,
+    get_request_data,
+    get_from,
+)
 from plenum.common.types import f
 from plenum.server.database_manager import DatabaseManager
 from plenum.server.request_handlers.nym_handler import NymHandler as PNymHandler
@@ -21,8 +27,12 @@ from plenum.server.request_handlers.utils import nym_to_state_key, get_nym_detai
 class NymHandler(PNymHandler):
     state_serializer = domain_state_serializer
 
-    def __init__(self, config, database_manager: DatabaseManager,
-                 write_req_validator: WriteRequestValidator):
+    def __init__(
+        self,
+        config,
+        database_manager: DatabaseManager,
+        write_req_validator: WriteRequestValidator,
+    ):
         super().__init__(config, database_manager)
         self.write_req_validator = write_req_validator
 
@@ -34,19 +44,23 @@ class NymHandler(PNymHandler):
         if isinstance(nym, str):
             nym = nym.strip()
         if not nym:
-            raise InvalidClientRequest(identifier, req_id,
-                                       "{} needs to be present".
-                                       format(TARGET_NYM))
+            raise InvalidClientRequest(
+                identifier, req_id, "{} needs to be present".format(TARGET_NYM)
+            )
         if not Authoriser.isValidRole(role):
-            raise InvalidClientRequest(identifier, req_id,
-                                       "{} not a valid role".
-                                       format(role))
+            raise InvalidClientRequest(
+                identifier, req_id, "{} not a valid role".format(role)
+            )
 
-    def additional_dynamic_validation(self, request: Request, req_pp_time: Optional[int]):
+    def additional_dynamic_validation(
+        self, request: Request, req_pp_time: Optional[int]
+    ):
         self._validate_request_type(request)
         operation = request.operation
 
-        nym_data = self.database_manager.idr_cache.getNym(operation[TARGET_NYM], isCommitted=False)
+        nym_data = self.database_manager.idr_cache.getNym(
+            operation[TARGET_NYM], isCommitted=False
+        )
         if not nym_data:
             # If nym does not exist
             self._validate_new_nym(request, operation)
@@ -62,8 +76,7 @@ class NymHandler(PNymHandler):
     def update_state(self, txn, prev_result, request, is_committed=False):
         self._validate_txn_type(txn)
         nym = get_payload_data(txn).get(TARGET_NYM)
-        existing_data = get_nym_details(self.state, nym,
-                                        is_committed=is_committed)
+        existing_data = get_nym_details(self.state, nym, is_committed=is_committed)
         txn_data = get_payload_data(txn)
         new_data = {}
         if not existing_data:
@@ -87,25 +100,43 @@ class NymHandler(PNymHandler):
         identifier, req_id, _ = get_request_data(request)
         role = operation.get(ROLE)
 
-        nym_data = self.database_manager.idr_cache.getNym(request.identifier, isCommitted=False)
+        nym_data = self.database_manager.idr_cache.getNym(
+            request.identifier, isCommitted=False
+        )
         if not nym_data:
             # Non-ledger nym case. These two checks duplicated and mainly executed in client_authn,
             # but it has point to repeat them here, for clear understanding of validation non-ledger request cases.
             if request.identifier != request.operation.get(TARGET_NYM):
-                raise InvalidClientRequest(identifier, req_id, "DID which is not stored on ledger can "
-                                                               "send nym txn only if appropriate auth_rules set "
-                                                               "and sender did equal to destination nym")
+                raise InvalidClientRequest(
+                    identifier,
+                    req_id,
+                    "DID which is not stored on ledger can "
+                    "send nym txn only if appropriate auth_rules set "
+                    "and sender did equal to destination nym",
+                )
             if not request.operation.get(VERKEY):
-                raise InvalidClientRequest(identifier, req_id, "Non-ledger nym txn must contain verkey for new did")
+                raise InvalidClientRequest(
+                    identifier,
+                    req_id,
+                    "Non-ledger nym txn must contain verkey for new did",
+                )
 
-        self.write_req_validator.validate(request,
-                                          [AuthActionAdd(txn_type=NYM,
-                                                         field=ROLE,
-                                                         value=role)])
+            if not self._is_self_certifying(
+                request.operation.get(TARGET_NYM), request.operation.get(VERKEY)
+            ):
+                raise InvalidClientRequest(
+                    identifier, req_id, "DID must be self-certifying."
+                )
+
+        self.write_req_validator.validate(
+            request, [AuthActionAdd(txn_type=NYM, field=ROLE, value=role)]
+        )
 
     def _validate_existing_nym(self, request, operation, nym_data):
         origin = request.identifier
-        owner = self.database_manager.idr_cache.getOwnerFor(operation[TARGET_NYM], isCommitted=False)
+        owner = self.database_manager.idr_cache.getOwnerFor(
+            operation[TARGET_NYM], isCommitted=False
+        )
         is_owner = origin == owner
 
         updateKeys = [ROLE, VERKEY]
@@ -115,12 +146,18 @@ class NymHandler(PNymHandler):
                 updateKeysInOperationOrOwner = True
                 newVal = operation[key]
                 oldVal = nym_data.get(key)
-                self.write_req_validator.validate(request,
-                                                  [AuthActionEdit(txn_type=NYM,
-                                                                  field=key,
-                                                                  old_value=oldVal,
-                                                                  new_value=newVal,
-                                                                  is_owner=is_owner)])
+                self.write_req_validator.validate(
+                    request,
+                    [
+                        AuthActionEdit(
+                            txn_type=NYM,
+                            field=key,
+                            old_value=oldVal,
+                            new_value=newVal,
+                            is_owner=is_owner,
+                        )
+                    ],
+                )
         if not updateKeysInOperationOrOwner:
             raise InvalidClientRequest(request.identifier, request.reqId)
 
@@ -128,3 +165,7 @@ class NymHandler(PNymHandler):
         if encoded:
             return domain_state_serializer.deserialize(encoded)
         return None, None, None
+
+    # TODO - Implement
+    def _is_self_certifying(self, did, verkey):
+        return True
