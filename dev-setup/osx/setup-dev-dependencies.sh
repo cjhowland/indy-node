6db2d34fa1db5f1bff9efe2cffaf17b978addb8a
Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>
2021-09-16 14:51:46 +0200

    nym_txn tests work in progress
    Signed-off-by: Woerner Dominic (RBCH/PJ-IOT) <dominic.woerner2@ch.bosch.com>

--- dev-setup/osx/setup-dev-dependencies.sh
+++ dev-setup/osx/setup-dev-dependencies.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
- echo "Please specify indy-sdk version tag"
- echo "e.g ./setup-dev-dependencies.sh 1.6.7"
- exit 1
-fi
+# if [ "$#" -ne 1 ]; then
+#  echo "Please specify indy-sdk version tag"
+#  echo "e.g ./setup-dev-dependencies.sh 1.6.7"
+#  exit 1
+# fi
 
 indy_sdk_version=$1
 
-brew update
+#brew update
 
 echo 'Installing libsodium...'
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/65effd2b617bade68a8a2c5b39e1c3089cc0e945/Formula/libsodium.rb
+brew tap-new $USER/local-libsodium
+brew extract --version=1.0.12 libsodium $USER/local-libsodium
+brew install libsodium@1.0.12
+#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/65effd2b617bade68a8a2c5b39e1c3089cc0e945/Formula/libsodium.rb
 echo 'Installed libsodium'
 
 echo 'Installing RocksDB 5.8.8...'
-brew install https://gist.githubusercontent.com/faisal00813/4059a5b41c10aa87270351c4795af752/raw/551d4de01a83f884c798ec5c2cb28a1b15d04db8/rocksdb.rb
+brew tap-new $USER/local-rocksdb
+brew extract --version=5.8.8 rocksdb $USER/local-rocksdb
+brew install rocksdb@5.8.8
+#brew install https://gist.githubusercontent.com/faisal00813/4059a5b41c10aa87270351c4795af752/raw/551d4de01a83f884c798ec5c2cb28a1b15d04db8/rocksdb.rb
 echo 'Installing RocksDB...'
 
 echo 'Installing libindy...'
@@ -32,10 +38,9 @@ export RUST_LOG=indy=trace
 export RUST_TEST_THREADS=1
 export OPENSSL_DIR=$(brew --prefix openssl)
 pushd /tmp
-git clone https://github.com/hyperledger/indy-sdk.git
+git clone https://github.com/domwoe/indy-sdk.git
 pushd indy-sdk/libindy
-git fetch --all --tags --prune
-git checkout tags/v"${indy_sdk_version}"
+git checkout -b feature/did_indy
 cargo build --release
 cp target/release/libindy.dylib /usr/local/lib/
 popd
